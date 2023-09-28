61cc3281bfeb05a0ff7233a7c801773ae32e465d
Vakho-web <vakhogvishiani@gmail.com>
2023-09-28 23:19:19 +0400

    ADD basic structure and buttons HTML/CSS; ADD Movie constructor to JS

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+function Movie(name, director, actor, length, description) {
+    this.name = name,
+    this.director = director,
+    this.actor = actor,
+    this.length = length,
+    this.description = description,
+    this.info = function() {
+        console.log(`Movie name: ${this.name}, directed by ${this.director}, main actor: ${this.actor}, runtime: ${this.length}, description: ${this.description}}`)
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     
+    const btn = document.getElementById("searchMovie");
 
+    
 
 })
