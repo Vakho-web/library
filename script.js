89f4dd0b8f43228e902e6898a1c63777ce40be4e
Vakho-web <vakhogvishiani@gmail.com>
2023-11-19 21:51:15 +0400

    ADD function to delete the movie from the list

--- script.js
+++ script.js
@@ -21,36 +21,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const movieLength = document.getElementById("movLength");
     const description = document.getElementById("description");
     const newMovieForm = document.querySelector(".newMovie");
+    
 
     newForm.addEventListener("click", () => {
         newMovieForm.style.display = "block";
     });
-
-    function render() {
-        let movieList = document.querySelector(".movieList");
-        movieList.innerHTML = "";
-        for(let i = 0; i < movieLibrary.length; i++) {
-            let movie = movieLibrary[i];
-            let movieEl = document.createElement("div")
-            movieEl.innerHTML = `<div class="card">
-                                    <div class="card-header">
-                                        <h2>${movie.name}</h2>
-                                        <button class="delete-card"> X </button>
-                                    </div>
-                                    <div class="card-body">
-                                        <h3>Directed By ${movie.director}</h3>
-                                        <h3>Main Actors: ${movie.actor}</h3>
-                                        <h3>Length: ${movie.length}</h3>
-                                        <h3>Summary: ${movie.description}</h3>
-                                    </div>
-                                </div>`;
-            movieList.appendChild(movieEl);
-        }
-    }
-
+    
     newMovieBtn.addEventListener('click', createNewEntry);
-        function createNewEntry() {
-            if(movieName.value === "" || director.value === "" || actor.value === "" || movieLength.value === "" || description.value === "") {
+    
+    function createNewEntry() {
+        if(movieName.value === "" || director.value === "" || actor.value === "" || movieLength.value === "" || description.value === "") {
                 alert("fill all fields")
             } else {
                 const movieNameValue = movieName.value;
@@ -58,9 +38,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 const actorValue = actor.value;
                 const movieLengthValue = movieLength.value;
                 const descriptionValue = description.value;
-    
+                
                 const newMovie = new Movie(movieNameValue, directorValue, actorValue, movieLengthValue, descriptionValue);
-
+                
                 movieLibrary.push(newMovie)
     
 /*              const relevantName = movieNameValue.toLowerCase();
@@ -72,12 +52,38 @@ document.addEventListener('DOMContentLoaded', () => {
                 actor.value = "";
                 movieLength.value = "";
                 description.value = "";
-    
+                
                 console.log('new movie entry created')
                 render();
                 /* newMovieForm.style.display = "none"; */
+                
             }
         }
-   
-
 })
+
+    function render() {
+        let movieList = document.querySelector(".movieList");
+        movieList.innerHTML = "";
+        for(let i = 0; i < movieLibrary.length; i++) {
+            let movie = movieLibrary[i];
+            let movieEl = document.createElement("div")
+            movieEl.innerHTML = `<div class="card">
+                                    <div class="card-header">
+                                        <h2>${movie.name}</h2>
+                                        <button id="delete-card" onclick="deleteCard(${i})"> X </button>
+                                    </div>
+                                    <div class="card-body">
+                                        <h3>Directed By ${movie.director}</h3>
+                                        <h3>Main Actors: ${movie.actor}</h3>
+                                        <h3>Length: ${movie.length}</h3>
+                                        <h3>Summary: ${movie.description}</h3>
+                                    </div>
+                                </div>`;
+            movieList.appendChild(movieEl);
+        }
+    }
+        
+        function deleteCard(index) {
+            movieLibrary.splice(index, 1);
+            render();
+        }
